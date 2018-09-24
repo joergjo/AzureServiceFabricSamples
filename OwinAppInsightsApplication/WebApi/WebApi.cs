a9ed7d659c8e06f9c6b8763e28be2b59c030be8f
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2018-09-24 17:23:48 +0200

    Initial commit of Web API/OWIN/AppInsights sample.

---
+++ OwinAppInsightsApplication/WebApi/WebApi.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ApplicationInsights.ServiceFabric;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.Collections.Generic;
+using System.Fabric;
+
+namespace WebApi
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class WebApi : StatelessService
+    {
+        public WebApi(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            // Set context for Application Insights
+            FabricTelemetryInitializerExtension.SetServiceCallContext(this.Context);
+
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext => new OwinCommunicationListener(Startup.ConfigureApp, serviceContext, ServiceEventSource.Current, "ServiceEndpoint"))
+            };
+        }
+    }
+}
